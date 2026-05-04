e7a04f7da724c8750ff7b27cc7129ff5f159e0f9
Richard Mensah <119437925+Richard-Mensah@users.noreply.github.com>
2026-05-04 07:13:08 +0100

    commit

--- lib/db.ts
+++ lib/db.ts
@@ -2,6 +2,8 @@ import { Pool } from 'pg'
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  max: 1,
 })
 
 export default pool
